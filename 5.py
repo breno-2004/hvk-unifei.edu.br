407975df698e20a6419014ab1ef45abc3386649c
Ubuntu <ubuntu@ip-172-31-14-192.ec2.internal>
2020-10-31 21:51:44 +0000

    atividade 5

---
+++ 5.py
@@ -0,0 +1,94 @@
+import rospy
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+import tf
+import math
+
+
+kp = 1
+
+odom = Odometry()
+scan = LaserScan()
+
+rospy.init_node('cmd_node')
+
+# Auxiliar functions ------------------------------------------------
+def getAngle(msg):
+    quaternion = msg.pose.pose.orientation
+    quat = [quaternion.x, quaternion.y, quaternion.z, quaternion.w]
+    euler = tf.transformations.euler_from_quaternion(quat)
+    yaw = euler[2]*180.0/math.pi
+    return yaw
+
+# CALLBACKS ---------------------------------------------------------
+def odomCallBack(msg):
+    global odom
+    odom = msg
+    
+def scanCallBack(msg):
+    global scan
+    scan = msg
+#--------------------------------------------------------------------
+
+# TIMER - Control Loop ----------------------------------------------
+def timerCallBack(event):
+	#Estado 1,encontrando o angulo
+	scanmin=min(scan.ranges)
+	for i in range(0 , len(scan.ranges))
+		if scan.ranges(i) == scanmin
+			sp=i
+	print("Len:")
+	print(len(scan.ranges))
+	print("sp:")
+	print(sp)
+	print("angmin:")
+	print(scan.angle_min)
+	print("angmax:")
+	print(scan.angle_max)
+	'''
+	for i in range(0 , len(scan.ranges))
+		va = 1000
+		if scan.ranges(i) < va and scan.ranges(i) > 0
+			va = scan.rages(i)
+			sp = i
+	print("Len:")
+	print(len(scan.ranges))
+	print("va:")
+	print(va)
+	print("sp:")
+	print(sp)
+	print("angmin:")
+	print(scan.angle_min)
+	print("angmax:")
+	print(scan.angle_max)
+	'''
+	'''
+	#Estado 2,virando para o setpoint
+    yaw = getAngle(odom)
+    setpoint = sp
+    error = (setpoint - yaw)
+    
+    if abs(error) > 180:
+        if setpoint < 0:
+            error += 360 
+        else:
+            error -= 360
+        
+        P = kp*error
+        I = 0
+        D = 0
+        control = P+I+D
+
+    msg = Twist()
+    msg.linear.x = control
+    pub.publish(msg)
+    '''
+
+pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
+scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
+
+timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
+
+rospy.spin()
