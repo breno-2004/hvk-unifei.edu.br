a37527d44a4dc9b077a723045140898814390441
Ubuntu <ubuntu@ip-172-31-7-56.ec2.internal>
2020-11-02 22:58:19 +0000

    Atividades aula 5

--- 5.py
+++ 5.py
@@ -134,30 +134,30 @@ def timerCallBack(event):
 	pub.publish(msg)
 	'''
 	#Girando so com P deu certo
-	yaw = getAngle(odom) 
-	setpoint1 = sp
-	error1 = (setpoint1 - yaw)
+	if state==1:
+		yaw = getAngle(odom) 
+		setpoint1 = sp
+		error1 = (setpoint1 - yaw)
+		
+		if abs(error1) > 180:
+			if setpoint < 0:
+				error1 += 360 
+			else:
+				error1 -= 360
+		
+		P1 = kp*error1
+		I1 = 0
+		D1 = 0
+		
+		control1 = P1+I1+D1
+		msg1 = Twist()
+		msg1.angular.z = control1
+		pub.publish(msg1)
 	
-	if abs(error1) > 180:
-		if setpoint < 0:
-			error1 += 360 
-		else:
-			error1 -= 360
-	
-	P1 = kp*error1
-	I1 = 0
-	D1 = 0
-	
-	control1 = P1+I1+D1
-	msg1 = Twist()
-	msg1.angular.z = control1
-	pub.publish(msg1)
-
-	#Terminou de girar
-	if len(scan.ranges) > 0 and state == 1:
-		if error1==0:
-			state=2
-	'''
+		#Terminou de girar
+		if len(scan.ranges) > 0 and state == 1:
+			if error1==0:
+				state=2
 	if state == 2:
 		#Andando em direcao ao objeto(setpoint=50cm) so com P
 		setpoint2 = 0.5
@@ -182,7 +182,6 @@ def timerCallBack(event):
 	msg2 = Twist()
 	msg2.linear.x = control2
 	pub.publish(msg2)
-	'''
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
