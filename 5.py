d35773578d389462be2d1679699b645f07b7ef5a
Ubuntu <ubuntu@ip-172-31-7-56.ec2.internal>
2020-11-02 19:57:31 +0000

    Atividades aula 5

--- 5.py
+++ 5.py
@@ -7,7 +7,15 @@ import math
 
 
 kp = 1
-
+ki = 2
+kd = 3
+I1 = 0
+I2 = 0
+setpoint = 0
+error1 = 0
+old_error1 = 0
+error2 = 0
+old_error2 = 0
 odom = Odometry()
 scan = LaserScan()
 
@@ -30,12 +38,39 @@ def scanCallBack(msg):
     global scan
     scan = msg
 #--------------------------------------------------------------------
-
+#Função soma de string ------------------------------------------------
+def sum_digits_string(str1):
+    sum_digit = 0
+    for x in str1:
+        if x.isdigit() == True:
+            z = int(x)
+            sum_digit = sum_digit + z
+    return sum_digit
+#--------------------------------------------------------------------	
+#Somando ------------------------------------------------
+Soma=0
+frequencia=0
+periodo=0
+Soma=sum_digits_string(str('2016013120'))
+Soma=sum_digits_string(str('2017001339'))+Soma
+Soma=sum_digits_string(str('30550'))+Soma
+Soma=sum_digits_string(str('2017020610'))+Soma
+frequencia=(Soma/4)
+periodo=(1/frequencia)
+#--------------------------------------------------------------------	
 # TIMER - Control Loop ----------------------------------------------
 def timerCallBack(event):
+	global kp
+	global ki
+	global kd
+	global I1
+	global I2
+	global error1
+	global old_error1
+	global error2
+	global old_error2
 	sp=0
-	state=1
-	control=0
+	#Encontrando o setpoint do angulo
 	if len(scan.ranges) > 0:
 		scanmin=min(scan.ranges)
 		for i in range(len(scan.ranges)):
@@ -51,32 +86,59 @@ def timerCallBack(event):
 		print(scan.angle_min)
 		print("angmax:")
 		print(scan.angle_max)
-
-	#Estado 2,virando para o setpoint
+		print("Scan pos 0:")#na frente do robo
+		print(scan.ranges[0])
+	#Girando
 	yaw = getAngle(odom) 
-	setpoint = sp
-	error = (setpoint - yaw)
+	setpoint1 = sp
+	error1 = (setpoint1 - yaw)
     
-	if abs(error) > 180:
+	if abs(error1) > 180:
 		if setpoint < 0:
-			error += 360 
+			error1 += 360 
 		else:
-			error -= 360
+			error1 -= 360
         
-		P = kp*error
-		I = 0
-		D = 0
-		control = P+I+D
-
-	msg = Twist()
-	msg.angular.z = control
-	pub.publish(msg)
+		P1 = kp*error1
+		I1 = I1 + error1 * ki
+		D1 = (error1 - old_error1)*kd
+		control1 = P1+I1+D1
+		old_error1 = error1
+		
+	msg1 = Twist()
+	msg1.angular.z = control1
+	pub.publish(msg1)
+	
+	#Andando em direção ao objeto(setpoint=50cm)
+	setpoint2 = 0.5
+    
+	scan_len = len(scan.ranges)
+	if scan_len > 0:
+		read = min(scan.ranges[scan_len-1 : scan_len+1])
+	
+		error2 = -(setpoint2 - read)
+        
+		P2 = kp*error2
+		I2 = I2 + error2 * ki
+		D2 = (error2 - old_error2)*kd
+		control2 = P2+I2+D2
+		old_error2 = error2
+		if control2 > 1:
+		    control2 = 1
+		elif control2 < -1:
+		    control2 = -1
+	else:
+		control2 = 0        
+    
+	msg2 = Twist()
+	msg2.linear.x = control2
+	pub.publish(msg2)
 
 
 pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
 odom_sub = rospy.Subscriber('/odom', Odometry, odomCallBack)
 scan_sub = rospy.Subscriber('/scan', LaserScan, scanCallBack)
 
-timer = rospy.Timer(rospy.Duration(0.05), timerCallBack)
+timer = rospy.Timer(rospy.Duration(periodo), timerCallBack)
 
 rospy.spin()
